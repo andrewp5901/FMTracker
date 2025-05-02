ccb82d0309435049598253bc115264ba3758d8e7
andrewp5901 <130093325+andrewp5901@users.noreply.github.com>
2025-05-01 22:32:22 -0500

    set up mongodb connection.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 
 dotenv.config(); // Load API key securely
 const API_KEY = process.env.LASTFM_API_KEY;
+const MONGO_URI = process.env.MONGO_URI;
 
 // Initialize express
 const app = express();
@@ -12,6 +15,10 @@ const PORT = process.env.PORT || 5000;
 // Initalize cors
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 app.get('/api/ping', (req, res) => {
     res.json({ message: "Server is connected to Angular"})
